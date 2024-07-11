386cda2b8bccc6c6c7bc5b9305f6f5f690813cb3
kundzior <grzybek2413@gmail.com>
2024-07-11 15:17:06 +0200

    flask fix

--- garlight.py
+++ garlight.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from datetime import time
+import time
 import threading
 from bulbs import Bulbs
 
-app = Flask(__name__)
+
 bulbs = Bulbs()
 
 
@@ -14,11 +14,14 @@ def update_bulbs():
         time.sleep(300)
 
 
-@app.before_first_request
 def start_update_thread():
     thread = threading.Thread(target=update_bulbs)
     thread.daemon = True
     thread.start()
+
+
+app = Flask(__name__)
+start_update_thread()
 
 
 @app.route("/")
