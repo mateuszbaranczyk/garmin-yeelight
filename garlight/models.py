5c022fb6861c6d59a254e52a959db14a4f7e22a3
kundzior <grzybek2413@gmail.com>
2024-08-21 00:01:36 +0200

    model as dict

--- garlight/models.py
+++ garlight/models.py
@@ -9,3 +9,8 @@ class BulbModel(db.Model):
     id: Mapped[str] = mapped_column(unique=True)
     ip: Mapped[str] = mapped_column(unique=True)
     name: Mapped[str] = mapped_column(unique=True)
+
+    def as_dict(self):
+        dict_repr = self.__dict__
+        del dict_repr["_sa_instance_state"]
+        return dict_repr
