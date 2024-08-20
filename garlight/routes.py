19fb991d5f3cea86ffa89ebd63aebd23440bf1c5
kundzior <grzybek2413@gmail.com>
2024-08-21 00:01:47 +0200

    endpoint set name

--- garlight/routes.py
+++ garlight/routes.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, Response, make_response, jsonify
+from flask import Blueprint, Response, make_response, jsonify, request
 
 from garlight.bulbs import BulbException, HomeBulb
 from garlight.endpoints_definitions import definitions
 from garlight.logs import gunicorn_logger
 from garlight.database import db
 from garlight.models import BulbModel
+from http import HTTPStatus
 
 bulb = Blueprint("bulb", import_name=__name__)
 manage = Blueprint("manage", import_name=__name__)
@@ -44,7 +45,18 @@ def on_off(name: str):
 def list_devices():
     devices = db.session.execute(db.select(BulbModel)).scalars()
     result = {"devices": devices}
-    return jsonify(result)
+    return result
+
+
+@manage.route("/set-name/<string:id>")
+def set_name(id: str):
+    name = request.args.get("name", None)
+    if name:
+        bulb = db.one_or_404(db.select(BulbModel).filter_by(id=id))
+        bulb.name = name
+        db.session.commit()
+        return bulb.as_dict()
+    return {"msg": "provide name"}, HTTPStatus.BAD_REQUEST
 
 
 def change_request(bulb: HomeBulb) -> Response:
@@ -53,11 +65,11 @@ def change_request(bulb: HomeBulb) -> Response:
         response = create_response(msg)
         gunicorn_logger.info(f"{bulb.bulb_name} - {msg}")
     except BulbException:
-        response = create_response("ERROR", 500)
+        response = create_response("ERROR", HTTPStatus.INTERNAL_SERVER_ERROR)
     return response
 
 
-def create_response(msg: str, status: int = 200) -> Response:
+def create_response(msg: str, status: int = HTTPStatus.OK) -> Response:
     response = make_response(msg, status)
     response.mimetype = "text/plain"
     return response
