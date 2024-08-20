eca618a0f3841ea800f436c6e0bfc500f34b4114
kundzior <grzybek2413@gmail.com>
2024-08-20 20:58:09 +0200

    db with models

--- garlight/models.py
+++ garlight/models.py
@@ -1,14 +1,11 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
+from database import db
 
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
 
 class BulbModel(db.Model):
     __tablename__ = "bulbs"
 
+    pk: Mapped[int] = mapped_column(primary_key=True)
     id: Mapped[str] = mapped_column(unique=True)
+    ip: Mapped[str] = mapped_column(unique=True)
     name: Mapped[str] = mapped_column(unique=True)
