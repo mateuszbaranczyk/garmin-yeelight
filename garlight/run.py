343179ced78ec83222ae12d7d64147280f3003b9
kundzior <grzybek2413@gmail.com>
2024-08-20 18:41:11 +0200

    db init

--- garlight/run.py
+++ garlight/run.py
@@ -2,13 +2,19 @@ from flask import Flask
 
 from garlight.logs import gunicorn_logger
 from garlight.scheduler import scheduler
-
+from models import db
 
 def create_app():
     app = Flask(__name__)
 
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///garlight.db"
+
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
     from garlight import routes  # noqa
 
