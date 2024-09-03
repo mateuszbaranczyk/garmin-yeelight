d1116a410ab30103394ccc458fa26d5dd7555ff7
kundzior <grzybek2413@gmail.com>
2024-09-03 17:06:53 +0200

    example bulb discover for testing

---
+++ garlight/tests/example.py
@@ -0,0 +1,20 @@
+bulbs_doscovery = [
+    {
+        "ip": "10.5.0.1",
+        "port": 55422,
+        "capabilities": {
+            "id": "0x00000000tes5t",
+            "model": "color",
+            "fw_ver": "65",
+            "support": "get_prop set_default set_power toggle set_bright start_cf stop_cf set_scene cron_add cron_get cron_del set_ct_abx set_rgb set_hsv set_adjust adjust_bright adjust_ct adjust_color set_music set_name",
+            "power": "on",
+            "bright": "1",
+            "color_mode": "2",
+            "ct": "1776",
+            "rgb": "16737792",
+            "hue": "24",
+            "sat": "100",
+            "name": "",
+        },
+    }
+]
