578432a68fb1edacd99e8c2e9e92aab2bc8fd813
kundzior <grzybek2413@gmail.com>
2024-09-04 17:41:07 +0200

    new functions registered

--- garlight/endpoints_definitions.py
+++ garlight/endpoints_definitions.py
@@ -1,5 +1,5 @@
 def create_definitions(devices: list[str]) -> str:
-    functions = ["on-off", "timer"]
+    functions = ["on-off", "set_timer", "set_warm", "set_color"]
     definitions = "- all, All\n" + "".join(
         f"-- {device.lower()},{device.capitalize()}\n" +
         "".join(f"--- {device}_{func},{func},{func.lower()}/{device.lower()}\n"
