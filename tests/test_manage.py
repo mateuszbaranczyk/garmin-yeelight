5380f410440d5e940c92227fcc9c76342534661e
kundzior <grzybek2413@gmail.com>
2024-09-05 17:49:19 +0200

    test set name

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -31,3 +31,21 @@ def test_list_devices(app):
 
     devices = response.json["devices"]
     assert len(devices) == num_of_devices
+
+
+def test_set_name(app):
+    old_name = "test"
+    new_name = "new_name"
+    num_of_devices = 1
+    request_data = {"name": new_name}
+    expected = {"name": new_name}
+
+    with app.app_context():
+        client = app.test_client()
+        bulbs = create_bulbs(num_of_devices, old_name)
+        name = bulbs[0]["name"]
+        response = client.post(f"/set-name/{name}", json=request_data)
+
+    assert response.json == expected
+
+
