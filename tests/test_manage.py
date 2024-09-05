f28f2fc947385dd4cc80306a5f76a306b40d9640
kundzior <grzybek2413@gmail.com>
2024-09-05 17:48:22 +0200

    test discover

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -1,8 +1,24 @@
-# list_devices
-# set_name
-# discover
+from unittest.mock import patch
+from tests.example import bulbs_single_discover, bulbs_multi_discover
+from garlight.db.database import db
+from garlight.db.models import BulbModel
+import pytest
 
 
 def test_smoke(client):
     response = client.get("/")
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "discover_result", [bulbs_multi_discover, bulbs_single_discover]
+)
+@patch("garlight.bulbs.discover_bulbs")
+def test_discover(discover_bulbs_mock, discover_result, client):
+    discover_bulbs_mock.return_value = discover_result
+    with client:
+        response = client.get("/discover")
+        result = db.session.query(BulbModel).all()
+
+    assert response.status_code == 302
+    assert len(result) == len(discover_result)
