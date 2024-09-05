39e8f690ab3eb904c99f5b50911d7dc7289feae7
kundzior <grzybek2413@gmail.com>
2024-09-05 17:48:04 +0200

    new conftest app config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,37 @@
-from garlight.run import create_app
 from pytest import fixture
 from flask.testing import FlaskClient
+from flask import Flask
+from garlight.db.database import db
+from garlight.db.models import BulbModel
 
 
 @fixture()
 def app():
-    app = create_app()
-    app.config.update(
-        {"TESTING": True, "SQLALCHEMY_DATABASE_URI": "sqlite://"}
-    )
+    app = Flask("test_app")
 
-    # other setup can go here
+    app.config["TESTING"] = True
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    from garlight.routing import bulbs_routes  # noqa
+    from garlight.routing import manage_routes  # noqa
+
+    app.register_blueprint(manage_routes.manage)
+    app.register_blueprint(manage_routes.root)
+    app.register_blueprint(bulbs_routes.bulb)
 
     yield app
-    # clean up / reset resources here
+
+    db.session.remove()
+    db.drop_all
 
 
 @fixture()
 def client(app) -> FlaskClient:
     return app.test_client()
+
+
+
