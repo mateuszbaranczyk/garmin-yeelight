73afc7754c6efc3f7735a46bac96f0b0715689e0
kundzior <grzybek2413@gmail.com>
2024-10-03 19:42:41 +0200

    set name test mod

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -35,18 +35,13 @@ def test_list_devices(app):
     assert len(devices) == num_of_devices
 
 
-def test_set_name(app):
-    old_name = "test"
+def test_set_name(client, bulb):
+    old_name = bulb.name
     new_name = "new_name"
-    num_of_devices = 1
     request_data = {"name": new_name}
     expected = {"name": new_name}
 
-    with app.app_context():
-        client = app.test_client()
-        bulbs = create_bulbs(num_of_devices, old_name)
-        name = bulbs[0]["name"]
-        response = client.post(f"/set-name/{name}", json=request_data)
+    response = client.post(f"/set-name/{old_name}", json=request_data)
 
     assert response.json == expected
 
